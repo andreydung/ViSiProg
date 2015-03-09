2095ff60e4b0ef337fb10314117e0e6b8c635690
Dung Nguyen <dzungng89@gmail.com>
2015-03-09 13:00:08 -0500

    80% done

--- interface/static/main.js
+++ interface/static/main.js
@@ -1,38 +1,98 @@
 var controller = (function () {
 
-  var taskpage;
+  var N_BATCH = 36;
+  var N_GROUP = 9;
+  var folderPath="/static/images/";
+  var listPath="/static/list.txt";
+  var listimage;
+  var N_TOTAL;
+  var prob = [];
+  var inPool = [];
+  var Nshown;
 
-	var N_BATCH = 36;
-	var N_GROUP = 9;
-	var folderPath="/static/images/";
-	var listPath="/static/list.txt";
-	var listimage;
-	var N_TOTAL;
+  var currentGroup = [];
+  var previousGroup = [];
 
-  var listshuffle = function(a) {
-    for (var j, x, i = a.length; i; j = Math.floor(Math.random() * i), x = a[--i], a[i] = a[j], a[j] = x);
-    return a;
-  };
+  var pressAllowed = true;
 
-	return {
-		init: function(callback) {
-			localStorage.clear();
-			localStorage.setItem("groups","");
+  function initializeArray(value, len) {
+    // initialize an array of len with value
+    var arr = [];
+    for (var i = 0; i < len; i++) {
+      arr.push(value);
+    }
+    return arr;  
+  }
 
-			// Ajax request to read textfile
-			$.get(listPath, function(data){
-				listimage = data.split("\n");
-				N_TOTAL = listimage.length;
-				listimage = listshuffle(listimage);
+  function getRandomfromRange(min, max) {
+    return Math.random() * (max - min) + min;
+  }
 
-				for (i = 0; i < N_BATCH; i++) {
-				 // $("#batch").append('<li class="ui-widget-content ui-corner-tr"> <img src="images/001.png" width="128" height="128"> </li>');
-					$("#batch").append(
-						$("<li>").attr({'class': 'ui-widget-content ui-corner-tr'}).append(
-							$("<img>").attr({src: folderPath.concat(listimage[i])})));
-				};
+  function randomFromPool() {
+    // Pick a random image from the pool inPool, with probability prob.
+    var sum = 0;
+    for (var i = 0; i < N_TOTAL; i++) {
+      sum = sum + inPool[i] * prob[i];
+    }
+
+    var random = getRandomfromRange(0, sum);
+    var cum = 0;
+    for (var i = 0; i < N_TOTAL; i++) {
+      cum += inPool[i] * prob[i];
+      if (cum > random)
+        return i;
+    }
+    return N_TOTAL;
+  }
+
+  function intersect(a, b) {
+    // how many common elements between a and b
+    a.sort();
+    b.sort();
+
+    var i = 0, j = 0;
+
+    count = 0;
+    while (i < a.length && j < b.length) {
+      if (a[i] == b[j]) {
+        count += 1;
+        i += 1;
+        j += 1;
+      }
+      else {
+        if (a[i + 1] < b[j + 1])
+          i += 1;
+        else
+          j += 1;
+      }
+    }
+    return count;
+  }
+
+  return {
+    init: function(callback) {
+      localStorage.clear();
+      localStorage.setItem("groups","");
+
+      // Ajax request to read textfile
+      $.get(listPath, function(data){
+        listimage = data.split("\n");
+        N_TOTAL = listimage.length;
+        Nshown = 0;
+
+        prob = initializeArray(1, N_TOTAL);
+        inPool = initializeArray(1, N_TOTAL);
+
+        for (i = 0; i < N_BATCH; i++) {
+          var tmp = randomFromPool();
+         // $("#batch").append('<li class="ui-widget-content ui-corner-tr"> <img src="images/001.png" width="128" height="128"> </li>');
+          $("#batch").append(
+            $("<li>").attr({'class': 'ui-widget-content ui-corner-tr'}).append(
+              $("<img>").attr({src: folderPath.concat(listimage[tmp]), 'id': tmp})));
+        };
+
         callback();
-			})
+      })
     },
 
     makeUIactions: function() {
@@ -81,39 +141,79 @@ var controller = (function () {
       $(document).on("mousedown", function () {
         if ($('#group ul li').length >= N_GROUP) {
           $group.droppable("disable");
-          console.log("Disabled")
         }
         else {
           $group.droppable("enable");
-          console.log("Enabled")
       }
       });
 
-      document.getElementById("ShuffleButton").addEventListener("click", ShuffleBatch);
-			// $("#ShuffleButton").on("click", ShuffleBatch);
+      // document.getElementById("ShuffleButton").addEventListener("click", ShuffleBatch);
+      $("#ShuffleButton").on("click", ShuffleBatch);
+      $(window).keypress(function(e) {
+        e.preventDefault();
+        if (!pressAllowed) 
+          return;
+        pressAllowed = false;
+        if (e.keyCode == 0 || e.keyCode == 32) {
+          ShuffleBatch();  
+        }
+      });
 
-			function ShuffleBatch() {
-				// reset group and store result
-				if ($('#group ul li').length < N_GROUP)
-				{
-					alert(" You must put 9 images in the group box!");
-				}
-				else
-				{
-					var imagegroup = $("#group img").map(function(){
-						return $(this).attr("src").split("/").pop();				
-					}).get();
-					console.log(imagegroup.toString());
-					console.log(imagegroup);
+      $(window).keyup(function(e) {
+        pressAllowed = true;
+      });
+      
+      function ShuffleBatch() {
+        // reset group and store result
+        if ($('#group ul li').length < N_GROUP)
+        {
+          alert(" You must put 9 images in the group box!");
+        }
+        else
+        {
+          previousGroup = currentGroup;
+          var currentGroup = $("#group img").map(function(){
+            return $(this).attr("id");        
+          }).get();
 
-					var tmp = localStorage.getItem("groups").toString();
-					localStorage.setItem("groups", tmp.concat(imagegroup.toString().concat(", end, ")));
-				}
-			}
+          // // Check how much overlap with previous group
+          // if (intersect(previousGroup, currentGroup) < N_GROUP/2) {
+          //   $("group ul").remove();
+          // }
 
-			function Submit() {
-				// send result to server
-			}
-		}
-	};
+          // Remove shown image in batch from the pool
+          $("#batch img").map(function() {
+            console.log("Show this image")
+            console.log($(this).attr("id"));
+            console.log(inPool[$(this).attr("id")]);
+            if (inPool[$(this).attr("id")] === 1)
+              Nshown += 1;
+            inPool[$(this).attr("id")] = 0;
+          })
+          
+
+          $("#progress").text(Nshown.toString());
+
+          // Shuffle
+          var imgBatch = $("#batch img");
+          $.each(imgBatch, function() {
+            var tmp = randomFromPool();
+            $(this).attr({"src": folderPath.concat(listimage[tmp]),
+                            "id": tmp})
+          })
+
+          // Store selection to local web storage
+          
+          console.log(currentGroup.toString());
+
+          var tmp = localStorage.getItem("groups").toString();
+          localStorage.setItem("groups", tmp.concat(currentGroup.toString().concat(",end,")));
+        }
+      }
+
+      function Submit() {
+        // send result to server
+      }
+    }
+  };
 }) ();
